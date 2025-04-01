525a17ee969db811545ae2f2102fb1d858de846c
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2025-03-31 23:29:22 -0500

    feat: implement case to android/ios pagging component

---
+++ composeApp/src/androidMain/kotlin/com/gondroid/picklerick/ui/core/components/LazyRowTarget.android.kt
@@ -0,0 +1,18 @@
+package com.gondroid.picklerick.ui.core.components
+
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.paging.compose.LazyPagingItems
+
+@Composable
+actual fun <T : Any> LazyRowTarget(
+    pagingItems: LazyPagingItems<T>, itemView: @Composable (T) -> Unit
+) {
+    LazyRow {
+        items(pagingItems.itemCount){ pos ->
+            pagingItems[pos]?.let { item ->
+                itemView(item)
+            }
+        }
+    }
+}
