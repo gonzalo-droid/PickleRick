057fcb4bdabec168f60f5e7ac8614d6805d0a1f9
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2025-04-01 23:58:06 -0500

    feat: DI to room android/ios

--- composeApp/src/iosMain/kotlin/com/gondroid/picklerick/di/DIConfigurator.ios.kt
+++ composeApp/src/iosMain/kotlin/com/gondroid/picklerick/di/DIConfigurator.ios.kt
@@ -1,9 +1,12 @@
 package com.gondroid.picklerick.di
 
+import com.gondroid.picklerick.data.database.RickMortyDatabase
+import com.gondroid.picklerick.data.database.getDatabase
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module {
     return module {
+        single<RickMortyDatabase> { getDatabase() }
     }
 }
