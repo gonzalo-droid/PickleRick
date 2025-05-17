83ed04d782e9ae8839a93d9d214018c1b4727a9b
Gonzalo Lozg <gonzalo.lozg@gmail.com>
2025-05-17 02:19:13 -0500

    feat: shared class to deskTop target

---
+++ composeApp/src/desktopMain/kotlin/com/gondroid/picklerick/di/DIConfigurator.desktop.kt
@@ -0,0 +1,11 @@
+package com.gondroid.picklerick.di
+
+import com.gondroid.picklerick.data.database.getDatabase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual fun platformModule(): Module {
+    return module {
+      single { getDatabase() }
+    }
+}
